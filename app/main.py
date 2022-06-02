497094cdda1b04ee3aa8fbf672336612573f9df9
Dev Watts <devwatts5@gmaiil.com>
2022-06-02 22:48:33 +0530

    final push

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from flask import Flask
+import random
+
 app= Flask(__name__)
+
 @app.route('/')
 def index():
-  return "<h1>Welcome to CodingX</h1>"
+  my_list = ["scissor", "paper", "rock"]
+  return random.choice(my_list)
