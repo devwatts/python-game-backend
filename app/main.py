88b5f1a4317b39102b0efdd8616ad231d360e618
Dev Watts <devwatts5@gmaiil.com>
2022-06-02 23:37:35 +0530

    final push

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
-import random
 from flask import Flask
-from flask_cors import CORS, cross_origin
-app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+from flask_cors import CORS
+import random
 
-@app.route("/")
-@cross_origin()
+app= Flask(__name__)
+CORS(app)
+
+@app.route('/')
 def index():
   my_list = ["scissor", "paper", "rock"]
   return random.choice(my_list)
